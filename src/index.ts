cb41d1bc023b2ce5d399c0cb1613e2fd0ca834ce
AhmadHddad <33379511+AhmadHddad@users.noreply.github.com>
2023-12-22 14:48:54 +0200

    docs

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,62 @@ import { Page, PuppeteerLifeCycleEvent, executablePath } from 'puppeteer';
 import { addHttpToURL, isURL } from 'hd-utils';
 
 export type HdHtml2PdfParams = {
+  /**
+   * @description url of a web page
+   */
   url?: string;
+  /**
+   * @description page default width including how the web page is viewed
+   * @default 1280
+   */
   width?: number;
+  /**
+   * @description the width of the view port for the page, if small it will look like a mobile device.
+   * @default 1280
+   */
+  viewPortWidth?: number;
+  /**
+   * @description the actual pdf width
+   * @default 1280
+   */
+  pdfWidth?: number;
+  /**
+   * @description HTML string that will be converted to PDF
+   */
   html?: string;
+  /**
+   * @description what type of emulation media, either a screen or print
+   * @default screen
+   */
   emulationMediaType?: 'screen' | 'print';
+  /**
+   * @description will get the puppeteer Page object
+   */
   getPage?: (page: Page) => Promise<void>;
-  waitUntil?: PuppeteerLifeCycleEvent;
-  headers?: Record<string, string>;
+  /**
+   * @description will wait until all of the passed events are successful.
+   * @default networkidle0
+   */
+  waitUntil?: PuppeteerLifeCycleEvent | PuppeteerLifeCycleEvent[];
+  /**
+   * @description http headers that will be passed to the web page.
+   */
+  httpHeaders?: Record<string, string>;
+  /**
+   * @description a function that will run in the context of the web page.
+   */
   pageFunction?: Parameters<Page['evaluate']>[0];
   fileName?: string;
+  /**
+   * @description height of the pdf file, the default is to wait until the pdf is loaded and then calculate the height so that it will be in a single PDF page.
+   * @default bodyHeight
+   */
   pdfHeight?: 'bodyHeight' | number;
+  /**
+   * @description number of pixels that will be added to the end of the PDF page as safe padding, this applies only for pdfHeight:bodyHeight
+   * @default 100
+   */
+  padding?: number;
 };
 
 export default async function hdHtml2Pdf({
@@ -20,10 +66,13 @@ export default async function hdHtml2Pdf({
   url,
   fileName,
   pageFunction,
+  pdfWidth = 1280,
   pdfHeight = 'bodyHeight',
   getPage,
-  headers,
+  httpHeaders,
+  viewPortWidth = 1280,
   width = 1280,
+  padding = 100,
   emulationMediaType = 'screen',
   waitUntil = 'networkidle0',
 }: HdHtml2PdfParams): Promise<Buffer> {
@@ -54,7 +103,7 @@ export default async function hdHtml2Pdf({
   const page = await browser.newPage();
 
   await page.setViewport({
-    width,
+    width: viewPortWidth ?? width,
     height: 720,
     hasTouch: false,
     isMobile: false,
@@ -62,8 +111,8 @@ export default async function hdHtml2Pdf({
   });
   // Website URL to export as pdf
 
-  if (headers) {
-    await page.setExtraHTTPHeaders(headers);
+  if (httpHeaders) {
+    await page.setExtraHTTPHeaders(httpHeaders);
   }
 
   if (url) {
@@ -94,9 +143,11 @@ export default async function hdHtml2Pdf({
   // Generate a PDF with a custom size
   return await page.pdf({
     path: fileName,
-    width: `${width}px`,
+    width: `${pdfWidth ?? width}px`,
     height:
-      pdfHeight === 'bodyHeight' ? `${bodyHeight + 100}px` : `${pdfHeight}px`, // Height of the entire webpage
+      pdfHeight === 'bodyHeight'
+        ? `${bodyHeight + padding}px`
+        : `${pdfHeight}px`, // Height of the entire webpage
     printBackground: true, // Print background graphics
   });
 }
